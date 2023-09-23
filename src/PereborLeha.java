37bfc69c21b46dfd7e256f52c8c93b7eadf71d89
Nataly <Max161161161@mail.ru>
2023-09-23 13:09:52 +0300

    END!!!!!!

--- src/PereborLeha.java
+++ src/PereborLeha.java
@@ -9,31 +9,33 @@ public class PereborLeha {
         public static void main(String[] args) {
             recBin(4);
         }
-           public static int recBin(int i){
+           public static void recBin(int i){
 
                if(Arrays.equals(arr,end)){
-                   return 1;
+                   return ;
                }
+
 
                if(arr[i]==1){
                    arr[i]=0;
 
 
                     i--;
-                   return recBin(i);
+                   recBin(i);
+                   return;
                }
-
                else
                {
                    arr[i]=1;
                    i=4;
                }
 
+
                 for (int a: arr) {
                     System.out.print(a);
                 }
                 System.out.println();
 
-            return  recBin(i);
+               recBin(i);
             }
 }
