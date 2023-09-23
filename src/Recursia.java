931fedd9698c7ed95e7ee407b5b42951e3ea208e
Nataly <Max161161161@mail.ru>
2023-09-23 03:03:25 +0300

    END!!!!!!

--- src/Recursia.java
+++ src/Recursia.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Recursia {
     static Scanner in =new Scanner(System.in);
-    private static int sumJo = in.nextInt(),count = in.nextInt();
+    private static final int sumJo = in.nextInt(),count = in.nextInt();
     private static int [] bank = new int[count*2];
     static int i=0 ,result=0;
-    static boolean[] bol = new boolean[count*2];
+    static boolean tmp = true;
+
+    static List <Integer> win = new ArrayList<>();
+
+
     public static void main(String[] args) {
         init(bank.length);
         getBank();
@@ -34,24 +41,8 @@ public class Recursia {
 
 
     public static int raidBank(int c,int z ,int x) {
-/*
-        if(t<0){
-            return -1;
-        }
-        if(result>sumJo || c<0){
-            result=0;
-            System.out.println("1");
-            c = t;
 
-            t--;
-        }
-        if (result<sumJo){
-            result+=bank[c];
-        }
-        if(result==sumJo){
 
-            return result;
-        }*/
         if(x==0){
             return -1;
         }
@@ -70,17 +61,31 @@ public class Recursia {
 
 
         if (result < sumJo){
+
+            if (tmp && x != bank.length-1)
+            {
+                result = bank[x];
+                win.add(result);
+                tmp=false;
+            }
+
             result += bank[c];
+
+            win.add(bank[c]);
             return raidBank(c-1,z,x);
 
-        }else if(result>sumJo)
+        }
+        else if(result>sumJo)
         {
+            win.clear();
+            tmp=true;
             result = bank[x];
             z--;
 
         }
 
         if(result==sumJo){
+            System.out.println(Arrays.toString(win.toArray()));
             return result;
         }
         if(x==0){
